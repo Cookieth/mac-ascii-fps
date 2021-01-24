bfda06e6bbb933356fa1fe472fe81a8974d22391
Keith <keithchoa@gmail.com>
2021-01-23 20:00:30 -0500

    Basic rudimentary environment

--- main.cpp
+++ main.cpp
@@ -12,6 +12,8 @@
 #include <algorithm>
 #include <unistd.h>
 
+#include <chrono>
+
 /*
 * An attempt to get this tutorial https://www.youtube.com/watch?v=xW8skO7MFYw to work on mac.
 */
@@ -37,8 +39,23 @@ void printScreen(std::string screenstr);
 int nScreenWidth = 120;
 int nScreenHeight = 40;
 
+float fPlayerX = 8.0f;
+float fPlayerY = 8.0f;
+float fPlayerA = 0.0f;
+
+int nMapHeight = 16;
+int nMapWidth = 16;
+
+// FOV set to 45˚
+float fFOV = 3.1415926 / 4.0;
+
+float fDepth = 16.0f;
+
 int main()
 {
+    // https://stackoverflow.com/questions/4804298/how-to-convert-wstring-into-string (keeping this here temporarily)
+    // setlocale(LC_ALL, "-");
+
     // Setup from tetris (TODO: Find documentation to understand this)
     setlocale(LC_CTYPE, "");
     initscr();
@@ -49,18 +66,203 @@ int main()
     start_color();
     use_default_colors();
 
-    //wchar_t *screen = new wchar_t[nScreenWidth * nScreenHeight];
-    const std::string screenstr = "  ┏━━k-vernooy/tetris━━┓\n  ┃                    ┃\n  ┃                    ┃   ┏━━next━━━┓\n  ┃                    ┃   ┃         ┃\n  ┃                    ┃   ┃         ┃\n  ┃                    ┃   ┃         ┃\n  ┃                    ┃   ┗━━━━━━━━━┛\n  ┃                    ┃\n  ┃                    ┃   ┏━━score━━┓\n  ┃                    ┃   ┃         ┃\n  ┃                    ┃   ┃  0      ┃\n  ┃                    ┃   ┃         ┃\n  ┃                    ┃   ┗━━━━━━━━━┛\n  ┃                    ┃\n  ┃                    ┃   ┏━━lines━━┓\n  ┃                    ┃   ┃         ┃\n  ┃                    ┃   ┃  0      ┃\n  ┃                    ┃   ┃         ┃\n  ┃                    ┃   ┗━━━━━━━━━┛\n  ┗━━━━━━━━━━━━━━━━━━━━┛\n                 ";
+    char *screen = new char[nScreenWidth * nScreenHeight];
+    //const std::string screenstr = "  ┏━━k-vernooy/tetris━━┓\n  ┃                    ┃\n  ┃                    ┃   ┏━━next━━━┓\n  ┃                    ┃   ┃         ┃\n  ┃                    ┃   ┃         ┃\n  ┃                    ┃   ┃         ┃\n  ┃                    ┃   ┗━━━━━━━━━┛\n  ┃                    ┃\n  ┃                    ┃   ┏━━score━━┓\n  ┃                    ┃   ┃         ┃\n  ┃                    ┃   ┃  0      ┃\n  ┃                    ┃   ┃         ┃\n  ┃                    ┃   ┗━━━━━━━━━┛\n  ┃                    ┃\n  ┃                    ┃   ┏━━lines━━┓\n  ┃                    ┃   ┃         ┃\n  ┃                    ┃   ┃  0      ┃\n  ┃                    ┃   ┃         ┃\n  ┃                    ┃   ┗━━━━━━━━━┛\n  ┗━━━━━━━━━━━━━━━━━━━━┛\n                 ";
+
+    // TODO: Load map from file (?)
+    std::string map;
+    map += "################";
+    map += "#..............#";
+    map += "#..............#";
+    map += "#..............#";
+    map += "#..............#";
+    map += "#...#..........#";
+    map += "#..............#";
+    map += "#..............#";
+    map += "#..............#";
+    map += "#...#..........#";
+    map += "#..............#";
+    map += "#..............#";
+    map += "#.........######";
+    map += "#..............#";
+    map += "#..............#";
+    map += "################";
+
+    auto tp1 = std::chrono::system_clock::now();
+    auto tp2 = std::chrono::system_clock::now();
 
     unsigned int microseconds = 10000;
     int count = 0;
 
-    while (count < 100)
+    while (true)
     {
-        usleep(microseconds);
-        //screen[nScreenWidth * nScreenHeight - 1] = '\0';
-        printScreen(screenstr);
-        count++;
+        // === Controls ===
+
+        tp2 = std::chrono::system_clock::now();
+        std::chrono::duration<float> elapsedTime = tp2 - tp1;
+        tp1 = tp2;
+        // My rendering print is not optimized, so I need to make it faster than the tutorial.
+        float fElapsedTime = 0.25f; //elapsedTime.count();
+
+        int ch = getch();
+        if (ch)
+        {
+            if (ch == (char)'a')
+            {
+                fPlayerA -= (0.8f) * fElapsedTime;
+            }
+            else if (ch == (char)'d')
+            {
+                fPlayerA += (0.8f) * fElapsedTime;
+            }
+            else if (ch == (char)'w')
+            {
+                // Multiply by unit vector for coordinates
+                fPlayerX += sinf(fPlayerA) * 5.0f * fElapsedTime;
+                fPlayerY += cosf(fPlayerA) * 5.0f * fElapsedTime;
+
+                //Convert location into integers, and check if the projected location hits a wall #
+                if (map.c_str()[(int)fPlayerX * nMapWidth + (int)fPlayerY] == '#')
+                {
+                    //Undo what we just did
+                    fPlayerX -= sinf(fPlayerA) * 5.0f * fElapsedTime;
+                    fPlayerY -= cosf(fPlayerA) * 5.0f * fElapsedTime;
+                }
+            }
+            else if (ch == (char)'s')
+            {
+                fPlayerX -= sinf(fPlayerA) * 5.0f * fElapsedTime;
+                fPlayerY -= cosf(fPlayerA) * 5.0f * fElapsedTime;
+
+                //Convert location into integers, and check if the projected location hits a wall #
+                if (map.c_str()[(int)fPlayerX * nMapWidth + (int)fPlayerY] == '#')
+                {
+                    //Undo what we just did
+                    fPlayerX += sinf(fPlayerA) * 5.0f * fElapsedTime;
+                    fPlayerY += cosf(fPlayerA) * 5.0f * fElapsedTime;
+                }
+            }
+        }
+
+        // === Rendering ===
+
+        // Horizontal calculations
+        // TODO: Implement newline \n character to prevent overflow
+        for (int x = 0; x < nScreenWidth; x++)
+        {
+            // 120 columns, means 120 rays to go across FOV
+            // Start angle = (fPlayerA - fFOV / 2.0f) (FOV to the right by half) and incremented
+            float fRayAngle = (fPlayerA - fFOV / 2.0f) + ((float)x / (float)nScreenWidth) * fFOV;
+
+            // Track distance to wall, keep incrementing until land in cell
+            float fDistanceToWall = 0;
+            bool bHitWall = false;
+
+            // unit vector in each direction
+            float fEyeX = sinf(fRayAngle);
+            float fEyeY = cosf(fRayAngle);
+
+            while (!bHitWall && fDistanceToWall < fDepth)
+            {
+                fDistanceToWall += 0.1f;
+                // Only require integer values
+                int nTestX = (int)(fPlayerX + fEyeX * fDistanceToWall);
+                int nTestY = (int)(fPlayerX + fEyeY * fDistanceToWall);
+
+                // Test if ray is out of bounds
+                if (nTestX < 0 || nTestX >= nMapWidth || nTestY < 0 || nTestY >= nMapHeight)
+                {
+                    bHitWall = true;
+                    fDistanceToWall = fDepth;
+                }
+                else
+                {
+                    //Convert 3D coords to 2D array
+                    if (map[nTestY * nMapWidth + nTestX] == '#')
+                    {
+                        bHitWall = true;
+                        //fDistanceToWall will retain it's value
+                    }
+                }
+            }
+
+            // Ceiling and floor filling
+            int nCeiling = (float)(nScreenHeight / 2.0) - nScreenHeight / ((float)fDistanceToWall);
+            int nFloor = nScreenHeight - nCeiling;
+
+            char nShade = ' ';
+
+            //Shade appropriately http://www.asciitable.com/
+            //Closest to furthest
+            //TODO: Figure out extended ascii
+            if (fDistanceToWall <= fDepth / 4.0f)
+            {
+                nShade = '#';
+            }
+            else if (fDistanceToWall < fDepth / 3.0f)
+            {
+                nShade = 'H';
+            }
+            else if (fDistanceToWall < fDepth / 2.0f)
+            {
+                nShade = '=';
+            }
+            else if (fDistanceToWall < fDepth)
+            {
+                nShade = '-';
+            }
+            else
+            {
+                nShade = '.';
+            }
+
+            // Draw Column
+            for (int y = 0; y < nScreenHeight; y++)
+            {
+                if (y <= nCeiling)
+                {
+                    screen[y * nScreenWidth + x] = ' ';
+                }
+                else if (y > nCeiling && y <= nFloor)
+                {
+                    screen[y * nScreenWidth + x] = nShade;
+                }
+                else
+                {
+                    // Taking a portion of how far the floor can be seen.
+                    float b = 1.0f - (((float)y - nScreenHeight / 2.0f) / ((float)nScreenHeight / 2.0f));
+                    if (b < 0.25)
+                    {
+                        nShade = 'X';
+                    }
+                    else if (b < 0.5)
+                    {
+                        nShade = '>';
+                    }
+                    else if (b < 0.75)
+                    {
+                        nShade = '\'';
+                    }
+                    else if (b < 0.9)
+                    {
+                        nShade = '`';
+                    }
+                    else
+                    {
+                        nShade = ' ';
+                    }
+                    screen[y * nScreenWidth + x] = nShade;
+                    //screen[y * nScreenWidth + x] = ' ';
+                }
+            }
+        }
+
+        screen[nScreenWidth * nScreenHeight - 1] = '\0';
+        printScreen(std::string(screen));
+
+        // Debugging things
+        //usleep(microseconds);
+        //count++;
     }
 
     // curses cleanup (TODO: Find documentation to understand this)
@@ -111,7 +313,7 @@ void printScreen(std::string screenstr)
     }
 
     //https://stackoverflow.com/questions/54684870/printw-and-mvwprintw-do-not-print
-    int ch = getch();
+    //int ch = getch();
 
     // loop through the window and print it
     for (int i = 0; i < window.size(); i++)
